07bf81c826bc456732e0461b7370d91e75919b31
milopo00 <71463869+milopo00@users.noreply.github.com>
2022-12-17 13:00:42 +0100

    Add files via upload

--- rfid.ino
+++ rfid.ino
@@ -1,63 +1,96 @@
 // La librería SPI.h nos permitirá establecer la comunicación entre la tarjeta ESP32 y el módulo.
 //y la librería MFRC522.h nos permitirá dialogar con el módulo
+
+//SDA --> G5
+//SCK --> G18
+//MOSI --> G23
+//MISO --> G19
+//GND --> GND
+//RST --> G27
+//3V3 --> 3V3
+
 #include <SPI.h>
 #include <MFRC522.h>
 
 #define SS_PIN 5
 #define RST_PIN 27
 
-//Parametros
-const int ipaddress[4] = {103, 97, 67, 25};
-
 //Variables
 byte nuidPICC[4] = {0, 0, 0, 0};
+String content = "";
+
 MFRC522::MIFARE_Key key;
 MFRC522 rfid = MFRC522(SS_PIN, RST_PIN);
 
 void setup() {
   //Init Serial USB
-  Serial.begin(38400);
-  Serial.println(F("Initialize System"));
-  //init rfid D8,D5,D6,D7
+  Serial.begin(230400);
   SPI.begin();
   rfid.PCD_Init();
-  //Serial.print(F("Reader :"));
-  //rfid.PCD_DumpVersionToSerial();
 
   Serial.println("Tap an RFID/NFC tag on the RFID-RC522 reader");
 }
-void loop() {
-  readRFID();
-}
-void readRFID(void ) { /* function readRFID */
-  ////Read RFID card
+
+String readRFID(){
+  String valores = "";
   for (byte i = 0; i < 6; i++) {
-      key.keyByte[i] = 0xFF;
+    key.keyByte[i] = 0xFF;
   }
   // Look for new 1 cards
   if ( ! rfid.PICC_IsNewCardPresent())
-      return;
+    return "";
   // Verify if the NUID has been readed
   if (  !rfid.PICC_ReadCardSerial())
-      return;
+    return "";
   // Store NUID into nuidPICC array
   for (byte i = 0; i < 4; i++) {
-      nuidPICC[i] = rfid.uid.uidByte[i];
+    nuidPICC[i] = rfid.uid.uidByte[i];
   }
   Serial.print(F("RFID In dec: "));
-  printDec(rfid.uid.uidByte, rfid.uid.size);
-  Serial.println();
+  for (byte i = 0; i < rfid.uid.size; i++){
+    valores.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    valores.concat(String(rfid.uid.uidByte[i],DEC));
+  }
   // Halt PICC
   rfid.PICC_HaltA();
   // Stop encryption on PCD
   rfid.PCD_StopCrypto1();
+  return valores;
 }
-/**
-    Helper routine to dump a byte array as dec values to Serial.
-*/
-void printDec(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-      Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-      Serial.print(buffer[i], DEC);
+
+void loop() {  ////Read RFID card
+  /*for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+  // Look for new 1 cards
+  if ( ! rfid.PICC_IsNewCardPresent())
+    return;
+  // Verify if the NUID has been readed
+  if (  !rfid.PICC_ReadCardSerial())
+    return;
+  // Store NUID into nuidPICC array
+  for (byte i = 0; i < 4; i++) {
+    nuidPICC[i] = rfid.uid.uidByte[i];
+  }
+  Serial.print(F("RFID In dec: "));
+  for (byte i = 0; i < rfid.uid.size; i++){
+    valores.concat(String(rfid.uid.uidByte[i] < 0x10 ? " 0" : " "));
+    valores.concat(String(rfid.uid.uidByte[i],DEC));
+  }
+  // Halt PICC
+  rfid.PICC_HaltA();
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+  Serial.println(valores);
+  Serial.println();
+  valores = "";*/
+  
+  content = readRFID();
+  if(content.equals("")){
+    return;
+  }else{
+    Serial.println(content);
+    Serial.println();
+    content = "";
   }
 }
